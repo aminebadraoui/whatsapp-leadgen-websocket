0e7835903acf7c84a367c92133a6735c9a36fac2
Amine Badraoui <aminebadraoui94@gmail.com>
2024-10-19 10:32:26 -0500

    set socket port to 3001

--- src/server.js
+++ src/server.js
@@ -83,7 +83,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
